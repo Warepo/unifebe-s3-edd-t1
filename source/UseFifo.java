c2f9592d4e8716fb3653022e83a3396c574b9a6b
odahcam <machado@odahcam.com>
2017-03-26 19:09:19 -0300

    formatted

--- source/UseFifo.java
+++ source/UseFifo.java
@@ -1,107 +1,110 @@
 import java.util.Scanner; // leitura em CLI (Command Line Interface)
 
+/**
+ * @author odahcam
+ * @author anologicon
+ */
 public class UseFifo {
 
-  public static void main(String[] args) {
-    Scanner io = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner io = new Scanner(System.in);
 
-    Fifo list = new Fifo();
-    int choice;
+        Fifo list = new Fifo();
+        int choice;
 
-    do {
-      System.out.print("\n╔═══════════════════\n"
-        + "║ Menu\n"
-        + "╠ \t1. Inserir\n"
-        + "╠ \t2. Retirar\n"
-        + "╠ \t3. Ver lista\n"
-        + "╠ \t4. Onde Está\n"
-        + "╠ \t5. Quem é\n"
-        + "╠ \t6. Cabeça\n"
-        + "╠ \t7. Detonar\n"
-        + "╠ \t8. Quantos\n"
-        + "╠ \t9. Vazar\n"
-        + "╚ Digite o número da ação desejada: ");
+        do {
+            System.out.print("\n╔═══════════════════\n"
+            + "║ Menu\n"
+            + "╠ \t1. Inserir\n"
+            + "╠ \t2. Retirar\n"
+            + "╠ \t3. Ver lista\n"
+            + "╠ \t4. Onde Está\n"
+            + "╠ \t5. Quem é\n"
+            + "╠ \t6. Cabeça\n"
+            + "╠ \t7. Detonar\n"
+            + "╠ \t8. Quantos\n"
+            + "╠ \t9. Vazar\n"
+            + "╚ Digite o número da ação desejada: ");
 
-      choice = io.nextInt();
+            choice = io.nextInt();
 
-      System.out.print("\n\n");
+            System.out.print("\n\n");
 
-      switch (choice) {
-        // 1. Inserir
-        case 1:
-          System.out.print("Informe a matrícula: ");
-          int id = io.nextInt();
-          System.out.println();
+            switch (choice) {
+                // 1. Inserir
+                case 1:
+                System.out.print("Informe a matrícula: ");
+                int id = io.nextInt();
+                System.out.println();
 
-          System.out.print("Informe o nome: ");
-          String name = io.next();
-          System.out.print("\n");
+                System.out.print("Informe o nome: ");
+                String name = io.next();
+                System.out.print("\n");
 
-          if (list.insert(id, name)) {
-            System.out.println("Funcionário inserido com sucesso!");
-          } else {
-            System.out.println("Fifo cheia - Overflow.");
-          }
+                if (list.insert(id, name)) {
+                    System.out.println("Funcionário inserido com sucesso!");
+                } else {
+                    System.out.println("Fifo cheia - Overflow.");
+                }
 
-          break;
-          
-        // 2. Retirar
-        case 2:
+                break;
 
-          if (list.remove() === false) {
-            System.out.println("Fifo vazia.");
-          } else {
-            System.out.println("Funcionário removido.");
-          }
+                // 2. Retirar
+                case 2:
 
-          break;
+                if (list.remove() === false) {
+                    System.out.println("Fifo vazia.");
+                } else {
+                    System.out.println("Funcionário removido.");
+                }
 
-        // 3. Ver Lista
-        case 3:
-          System.out.println("Veja a lista: ");
+                break;
 
-          list.show();
+                // 3. Ver Lista
+                case 3:
+                System.out.println("Veja a lista: ");
 
-          break;
+                list.show();
 
-        // 4. Onde Está
-        case 4:
-          System.out.println("Digite a matrícula do funcionario:");
-          id = io.nextInt();
+                break;
 
-          list.show_in(id);
+                // 4. Onde Está
+                case 4:
+                System.out.println("Digite a matrícula do funcionario:");
+                id = io.nextInt();
 
-          break;
+                list.show_in(id);
 
-        // 5. Quem é
-        case 5:
-          break;
+                break;
 
-        
-        // 6. Cabeça
-        case 6:
-          break;
+                // 5. Quem é
+                case 5:
+                break;
 
-        // 7. Detonar
-        case 7:
-          list.destroy();
-          break;
-        
-        // 8. Quantos
-        case 8:
-          break;
+                // 6. Cabeça
+                case 6:
+                break;
 
-        // 9. Vazar
-        case 9:
-          break;
+                // 7. Detonar
+                case 7:
+                list.destroy();
+                break;
 
-        // Não entendido
-        default:
-          System.out.println("Não foi possível identificar sua escolha, por favor, escolha novamente:\n");
-          break;
-      }
+                // 8. Quantos
+                case 8:
+                break;
 
-    } while (choice != 9);
+                // 9. Vazar
+                case 9:
+                break;
 
-  }
+                // Não entendido
+                default:
+                System.out.println("Não foi possível identificar sua escolha, por favor, escolha novamente:\n");
+                break;
+            }
+
+        } while (choice != 9);
+
+    }
 }
