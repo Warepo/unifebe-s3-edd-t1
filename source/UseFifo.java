ee36bb4416c863e5e2f8497c97986bb5a0f4ac9c
odahcam <machado@odahcam.com>
2017-03-29 21:52:23 -0300

    formated

--- source/UseFifo.java
+++ source/UseFifo.java
@@ -33,100 +33,100 @@ public class UseFifo {
             System.out.print("\n\n");
 
             switch (choice) {
-            // 1. Inserir
-            case 1:
-                System.out.print("Informe a matrícula: ");
-                id = io.nextInt();
+                // 1. Inserir
+                case 1:
+                    System.out.print("Informe a matrícula: ");
+                    id = io.nextInt();
 
-                System.out.print("Informe o nome: ");
-                name = io.next();
+                    System.out.print("Informe o nome: ");
+                    name = io.next();
 
-                System.out.print("\n");
+                    System.out.print("\n");
 
-                if (list.push(id, name)) {
-                    System.out.println("Funcionário inserido com sucesso!");
-                } else {
-                    System.out.println("Fifo cheia - Overflow.");
-                }
+                    if (list.push(id, name)) {
+                        System.out.println("Funcionário inserido com sucesso!");
+                    } else {
+                        System.out.println("Fifo cheia - Overflow.");
+                    }
 
-                System.out.print("\n");
+                    System.out.print("\n");
 
-                break;
+                    break;
 
-            // 2. Retirar
-            case 2:
+                // 2. Retirar
+                case 2:
 
-                if (list.pop() == false) {
-                    System.out.println("Fifo vazia.");
-                } else {
-                    System.out.println("Funcionário removido.");
-                }
-                System.out.println("");
+                    if (list.pop() == false) {
+                        System.out.println("Fifo vazia.");
+                    } else {
+                        System.out.println("Funcionário removido.");
+                    }
+                    System.out.println("");
 
-                break;
+                    break;
 
-            // 3. Ver Lista
-            case 3:
-                System.out.println("Veja a lista: ");
+                // 3. Ver Lista
+                case 3:
+                    System.out.println("Veja a lista: ");
 
-                list.show();
+                    list.show();
 
-                break;
+                    break;
 
-            // 4. Onde Está
-            case 4:
-                System.out.print("Digite a matrícula do funcionário:");
-                id = io.nextInt();
-                System.out.println();
+                // 4. Onde Está
+                case 4:
+                    System.out.print("Digite a matrícula do funcionário:");
+                    id = io.nextInt();
+                    System.out.println();
 
-                position = list.getPositionByID(id);
-                if (position > 0) {
-                    System.out.println("Posição: " + list.getPositionByID(id) + "°\n");
-                } else {
-                    System.out.println("Funcionário não encontrado.\n");
-                }
+                    position = list.getPositionByID(id);
+                    if (position > 0) {
+                        System.out.println("Posição: " + list.getPositionByID(id) + "°\n");
+                    } else {
+                        System.out.println("Funcionário não encontrado.\n");
+                    }
 
-                break;
+                    break;
 
-            // 5. Quem é
-            case 5:
-                System.out.print("Digite a matrícula do funcionário:");
-                id = io.nextInt();
-                name = list.getNameByID(id);
+                // 5. Quem é
+                case 5:
+                    System.out.print("Digite a matrícula do funcionário:");
+                    id = io.nextInt();
+                    name = list.getNameByID(id);
 
-                if (name != "") {
-                    System.out.println("Nome: " + list.getNameByID(id) + "\n");
-                } else {
-                    System.out.println("Funcionário não encontrado.\n");
-                }
+                    if (name != "") {
+                        System.out.println("Nome: " + list.getNameByID(id) + "\n");
+                    } else {
+                        System.out.println("Funcionário não encontrado.\n");
+                    }
 
-                break;
+                    break;
 
-            // 6. Cabeça
-            case 6:
-                System.out.print("O cabeça da lista é: ");
-                System.out.println(list.getHEAD() + "\n");
+                // 6. Cabeça
+                case 6:
+                    System.out.print("O cabeça da lista é: ");
+                    System.out.println(list.getHEAD() + "\n");
 
-                break;
+                    break;
 
-            // 7. Detonar
-            case 7:
-                list.destroy();
-                break;
+                // 7. Detonar
+                case 7:
+                    list.destroy();
+                    break;
 
-            // 8. Quantos
-            case 8:
-                System.out.println("A lista possui " + list.count() + " funcionários.\n");
-                break;
+                // 8. Quantos
+                case 8:
+                    System.out.println("A lista possui " + list.count() + " funcionários.\n");
+                    break;
 
-            // 9. Vazar
-            case 9:
-                break;
+                // 9. Vazar
+                case 9:
+                    break;
 
-            // Não entendido
-            default:
-                System.out.println("Não foi possível identificar sua escolha, por favor, escolha novamente:\n");
-                break;
+                // Não entendido
+                default:
+                    System.out.println("Não foi possível identificar sua escolha, por favor, escolha novamente:\n");
+                    break;
             }
 
         } while (choice != 9);
